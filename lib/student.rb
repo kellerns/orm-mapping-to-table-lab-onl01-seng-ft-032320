3e223e51eea03fe8ad0d35b971d28ee5c7fa82e3
Nicholas Keller <nic.keller328@gmail.com>
2020-04-26 21:32:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,4 @@ attr_reader :id
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
 end
