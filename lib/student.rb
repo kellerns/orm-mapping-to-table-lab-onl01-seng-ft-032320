523cd4541be0e5fe4f070f2f3f6d2632fa1ccfb5
Nicholas Keller <nic.keller328@gmail.com>
2020-04-26 21:32:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,21 @@ attr_reader :id
         DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
