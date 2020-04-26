d994b71984ca38e5837bbb00bcde8f78e6e7dc27
Nicholas Keller <nic.keller328@gmail.com>
2020-04-26 21:40:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ attr_reader :id
   end
 
   def self.create(student_hash)
-    student = Student.new(student_hash.name, student_hash.grade)
+    student = Student.new(student_hash[:name], student_hash[:grade])
     student.save
     student
   end
